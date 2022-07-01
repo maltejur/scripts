4d0e7d29d70febbf4aa64a53b9129e94f11e3540
Malte Jürgens <maltejur@dismail.de>
2022-07-01 16:17:22 +0200

    fix

--- create_timer.sh
+++ create_timer.sh
@@ -80,8 +80,7 @@ fi
 echo "-> Enabling timer and running it for the first time"
 journalctl -f -u ${name}.service --no-pager -n0 -o cat &
 journal_pid=$!
-systemctl enable --now ${name}.timer
-systemctl --wait ${name}.service
+systemctl --wait enable --now ${name}.timer
 kill $journal_pid
 
 if [[ $quiet == "false" ]]; then
